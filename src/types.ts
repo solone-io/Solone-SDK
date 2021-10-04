48b842420b83443da837ea79ca7f08898f7d541d
AQadir64 <a.qadir67@outlook.com>
2021-10-04 17:09:48 +0500

    changes: typescipt types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,24 @@
-import { ConfirmedTransaction } from "@solana/web3.js";
+import { ConfirmedTransaction, ConfirmedTransactionMeta, PublicKey, Transaction } from '@solana/web3.js';
 
-class TransactionWithSignature {
-  constructor(
-    public signature: string,
-    public confirmedTransaction: ConfirmedTransaction
-  ) {}
+export class TransactionWithSignature {
+  constructor(public signature: string, public confirmedTransaction: ConfirmedTransaction) {}
 }
 
-module.exports = { TransactionWithSignature };
+export type CreateAccountResponse = {
+  publicKey: PublicKey;
+  secretKey: Uint8Array;
+  stringPublicKey: string;
+  stringSecretKey: string;
+};
+
+export type TransactionObject = {
+  fees?: number;
+  amount?: number;
+  slot?: number;
+  transaction?: Transaction;
+  meta?: ConfirmedTransactionMeta | null;
+  blockTime?: number | null | undefined;
+  signature?: string;
+};
+
+export type Cluster = 'devnet' | 'testnet' | 'mainnet-beta';
